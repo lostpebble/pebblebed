d8bdb52c08dd3dec87841324b79b1a9c06b60a14
Paul Myburgh <paulmyburgh88@gmail.com>
2017-12-14 13:45:02 +0200

    Added queries and performance timing to allOperations

--- test/src/tests/_allOperations.ts
+++ test/src/tests/_allOperations.ts
@@ -2,6 +2,8 @@ import { IDSTestEntityIntId, TestEntityIntIdModel } from "../entities/TestEntity
 import { IDSTestEntityStringId, TestEntityStringIdModel } from "../entities/TestEntityStringId";
 import { inspect } from "util";
 import { Pebblebed } from "pebblebed";
+import { printMarkMeasurements, waitSeconds } from "../utility";
+const { performance } = require("perf_hooks");
 
 export async function runAllOperations() {
   const entityGenerated: IDSTestEntityIntId = {
@@ -22,13 +24,15 @@ export async function runAllOperations() {
     location: {
       latitude: 12.0,
       longitude: 12.0,
-    }
+    },
   };
 
   // Generated Int ID
   console.log("\nSAVE: Generated Int ID Entity\n");
+  performance.mark("START");
 
   let result = await TestEntityIntIdModel.save(entityGenerated).generateUnsetIds().run();
+  performance.mark("SAVE:GENERATED_INT_ID");
 
   console.log(inspect(result));
 
@@ -36,28 +40,69 @@ export async function runAllOperations() {
 
   // Deliberate Int ID
   console.log("\nSAVE: Set Int ID Entity\n", inspect(await TestEntityIntIdModel.save(entityIntegerId).run()));
+  performance.mark("SAVE:SET_INT_ID");
 
   // Mixed Generated Int ID and deliberate Int ID
-  console.log("\nSAVE: Mixed Generated / Set Int ID Entities\n", inspect(await TestEntityIntIdModel.save([entityGenerated, entityIntegerId]).generateUnsetIds().run()));
+  console.log(
+    "\nSAVE: Mixed Generated / Set Int ID Entities\n",
+    inspect(await TestEntityIntIdModel.save([entityGenerated, entityIntegerId]).generateUnsetIds().run())
+  );
+  performance.mark("SAVE:MIXED_GENERATED_SET_INT_IDS");
 
   // Deliberately set String ID
   console.log("\nSAVE: String ID Entity\n", inspect(await TestEntityStringIdModel.save(entityString).run()));
+  performance.mark("SAVE:STRING_ID");
 
   console.log("\nWaiting 2 seconds\n");
+  await waitSeconds(2);
+  performance.mark("WAITED_AFTER_SAVE");
 
-  console.log("\nLOAD: First Generated ID Entity\n", inspect(await TestEntityIntIdModel.load(firstGeneratedId).run()));
+  console.log(
+    "\nLOAD: First Generated ID Entity\n",
+    inspect(await TestEntityIntIdModel.load(firstGeneratedId).run())
+  );
+  performance.mark("LOAD:GENERATED_INT_ID");
 
   console.log("\nLOAD: Int ID Entity\n", inspect(await TestEntityIntIdModel.load(123).run()));
+  performance.mark("LOAD:SET_INT_ID");
 
   console.log("\nLOAD: String ID Entity\n", inspect(await TestEntityStringIdModel.load("123abc").run()));
+  performance.mark("LOAD:STRING_ID");
 
-  const loadKeys = [Pebblebed.key(TestEntityIntIdModel, firstGeneratedId), Pebblebed.key(TestEntityIntIdModel, 123)];
+  const loadKeys = [
+    Pebblebed.key(TestEntityIntIdModel, firstGeneratedId),
+    Pebblebed.key(TestEntityIntIdModel, 123),
+  ];
 
   console.log("\nCREATED KEYS:\n", inspect(loadKeys, false, 6));
 
-  console.log(loadKeys[0].path);
+  console.log(
+    "\nLOAD: Mixed Keys for both Set and Generated Int ID Entities\n",
+    inspect(await TestEntityIntIdModel.load(loadKeys).run())
+  );
+  performance.mark("LOAD:MIXED_GENERATED_SET_INT_IDS");
 
-  console.log("\nLOAD: Mixed Keys for both Set and Generated Int ID Entities\n", inspect(await TestEntityIntIdModel.load(loadKeys).run()));
+  console.log("\nWaiting 3 seconds\n");
+  await waitSeconds(3);
+  performance.mark("WAITED_AFTER_LOAD");
 
-  console.log("\nWaiting 2 seconds\n");
+  console.log(
+    `\nQUERY: Tag Int ID Entities`,
+    inspect(await TestEntityIntIdModel.query().filter("tags", "=", "blue").run())
+  );
+  performance.mark("QUERY:TAGS_INT_ID");
+
+  console.log(
+    `\nQUERY: Amount INT ID Entities, limited -> 3`,
+    inspect(await TestEntityIntIdModel.query().filter("amount", "<", 20).limit(3).run())
+  );
+  performance.mark("QUERY:AMOUNT_INT_ID");
+
+  console.log(
+    `\nQUERY: Date, String ID Entities, limited -> 3`,
+    inspect(await TestEntityStringIdModel.query().filter("date", "<", new Date()).limit(3).run())
+  );
+  performance.mark("QUERY:DATE_STRING_ID");
+
+  printMarkMeasurements(performance.getEntriesByType("mark"));
 }
--- test/src/utility.ts
+++ test/src/utility.ts
@@ -1,5 +1,28 @@
+const { performance } = require("perf_hooks");
+
 export async function waitSeconds(seconds: number) {
   return new Promise((resolve) => {
     setTimeout(() => resolve(true), seconds * 1000);
   });
+}
+
+export function printMarkMeasurements(markEntries: PerformanceEntry[]) {
+  if (!Array.isArray(markEntries) || markEntries.length === 0) {
+    console.log(`No performance (mark) entries passed to printMeasurements() ...`);
+    return false;
+  }
+
+  for (let i = 1; i < markEntries.length; i += 1) {
+    performance.measure(`TIME_TAKEN:${markEntries[i].name}`, markEntries[i - 1].name, markEntries[i].name);
+  }
+
+  const measurements: PerformanceEntry[] = performance.getEntriesByType("measure");
+
+  for (const measurement of measurements) {
+    console.log(`${measurement.name} : ${Math.round(measurement.duration)} ms`);
+  }
+
+  performance.measure("TOTAL_TIME", markEntries[0].name, markEntries[markEntries.length - 1].name);
+
+  console.log(`Total elapsed time from [${markEntries[0].name}] -> [${markEntries[markEntries.length - 1]}] : ${Math.round(performance.getEntriesByName("TOTAL_TIME")[0].duration)} ms`)
 }
