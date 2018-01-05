1df73f310164a81d1167b81333bde4f50f2adb6a
Paul Myburgh <paulmyburgh88@gmail.com>
2018-01-05 17:04:28 +0200

    Updates

---
+++ component---src-pages-page-2-js-03d3d956f4f89420cb21.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xc6c285f8fd10],{186:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var a=l(4),n=u(a),d=l(43),f=u(d);t.default=function(){return n.default.createElement("div",null,n.default.createElement("p",null,"Hello world from my second Gatsby page"),n.default.createElement(f.default,{to:"/"},"back home"))},e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-page-2-js-03d3d956f4f89420cb21.js.map
---
+++ path---about-css-modules-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x788aad75d9fd],{299:function(t,a){t.exports={pathContext:{}}}});
+//# sourceMappingURL=path---about-css-modules-a0e39f21c11f6a62c5ab.js.map
