d82a07012f13aa4dfc88b8e1ed18f4bccc9a6a7a
Paul Myburgh <paulmyburgh88@gmail.com>
2018-01-05 17:07:36 +0200

    [site] cleaned up some example files

--- gatsby-site/src/pages/page-2.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Link from "gatsby-link";
-
-export default () => (
-  <div>
-    <p>Hello world from my second Gatsby page</p>
-    <Link to="/">back home</Link>
-  </div>
-);
